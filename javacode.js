cc4100370901c0027a27ba14da9f8819da173e79
Danfall369 <Danfall963@gmail.com>
2023-03-15 03:13:40 -0400

    Working Menu on nav

--- javacode.js
+++ javacode.js
@@ -2,9 +2,9 @@ const logo = document.querySelector('.mobileMenu');
 const menu = document.querySelector('.mobilePop');
 
 logo.addEventListener('click', () => {
-    menu.classList.toggle('showmenu');
+  menu.classList.toggle('showmenu');
 });
 
 document.querySelectorAll('.mobileItem').forEach((n) => n.addEventListener('click', () => {
-    menu.classList.remove('showmenu');
+  menu.classList.remove('showmenu');
 }));
